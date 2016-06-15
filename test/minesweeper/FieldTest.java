56e649c4a9864f9a116246f4f49eaa8fd60dc486
Lukas Straka <lstraka91@gmail.com>
2016-06-15 15:52:00 +0200

    commit

---
+++ test/minesweeper/FieldTest.java
@@ -0,0 +1,87 @@
+package minesweeper;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import minesweeper.core.Field;
+import minesweeper.core.Mine;
+
+public class FieldTest {
+
+	@Test
+	public void testRowGetter() {
+		Random rnd = new Random();
+		int rows = rnd.nextInt(10) + 2;
+		int cols = rnd.nextInt(10) + 2;
+		int mines = rnd.nextInt(4) + 1;
+//		 System.out.println(rows+ " " + cols +" "+ mines );
+		Field field = new Field(rows, cols, mines);
+		assertEquals(rows, field.getRowCount());
+		
+	}
+	@Test
+	public void testCollGetter() {
+		Random rnd = new Random();
+		int rows = rnd.nextInt(10) + 2;
+		int cols = rnd.nextInt(10) + 2;
+		int mines = rnd.nextInt(4) + 1;
+//		 System.out.println(rows+ " " + cols +" "+ mines );
+		Field field = new Field(rows, cols, mines);
+		
+		assertEquals(cols, field.getColumnCount());
+	
+
+	}
+	@Test
+	public void testMineGetter() {
+		Random rnd = new Random();
+		int rows = rnd.nextInt(10) + 2;
+		int cols = rnd.nextInt(10) + 2;
+		int mines = rnd.nextInt(4) + 1;
+		 System.out.println(rows+ " " + cols +" "+ mines );
+		Field field = new Field(rows, cols, mines);
+		
+		assertEquals(mines, field.getMineCount());
+
+	}
+
+	@Test
+	public void testFieldFilled() {
+		Random rnd = new Random();
+		int rows = rnd.nextInt(10) + 2;
+		int cols = rnd.nextInt(10) + 2;
+		int mines = rnd.nextInt(4) + 1;
+		System.out.println(rows + " " + cols + " " + mines);
+		Field field = new Field(rows, cols, mines);
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				assertNotNull(field.getTile(i, j));
+			}
+		}
+	}
+
+	@Test
+	public void generateMinesTest() {
+		Random rnd = new Random();
+		int rows = rnd.nextInt(10) + 2;
+		int cols = rnd.nextInt(10) + 2;
+		int mines = rnd.nextInt(4) + 1;
+		System.out.println(rows + " " + cols + " " + mines);
+		Field field = new Field(rows, cols, mines);
+		int mineCount=0;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if(field.getTile(i, j)instanceof Mine){
+					mineCount++;
+				}
+			}
+		}
+//		System.out.println(field.getGeneratedMineCount());
+
+		assertEquals(mines, mineCount);
+	}
+
+}
