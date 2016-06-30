050c0a800790a1dd8196331b7456be31c0c86077
Lukas Straka <lstraka91@gmail.com>
2016-06-30 15:43:54 +0200

    30.6

---
+++ src/minesweeper/BestTimes.java
@@ -0,0 +1,89 @@
+package minesweeper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Player times.
+ */
+public class BestTimes implements Iterable<BestTimes.PlayerTime> {
+	/** List of best player times. */
+	private List<PlayerTime> playerTimes = new ArrayList<PlayerTime>();
+
+	/**
+	 * Returns an iterator over a set of best times.
+	 * 
+	 * @return an iterator
+	 */
+	public Iterator<PlayerTime> iterator() {
+		return playerTimes.iterator();
+	}
+
+	/**
+	 * Adds player time to best times.
+	 * 
+	 * @param name
+	 *            name to the player
+	 * @param time
+	 *            player time in seconds
+	 */
+	public void addPlayerTime(String name, int time) {
+		playerTimes.add(new PlayerTime(name, time));
+		Collections.sort(playerTimes);
+	}
+
+	/**
+	 * Returns a string representation of the object.
+	 * 
+	 * @return a string representation of the object
+	 */
+	public String toString() {
+		throw new UnsupportedOperationException(
+				"Method toString not yet implemented");
+	}
+
+	/**
+	 * Player time.
+	 */
+	public static class PlayerTime implements Comparable<PlayerTime> {
+		/** Player name. */
+		private final String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public int getTime() {
+			return time;
+		}
+
+		/** Playing time in seconds. */
+		private final int time;
+
+		/**
+		 * Constructor.
+		 * 
+		 * @param name
+		 *            player name
+		 * @param time
+		 *            playing game time in seconds
+		 */
+		public PlayerTime(String name, int time) {
+			this.name = name;
+			this.time = time;
+		}
+
+		@Override
+		public int compareTo(PlayerTime o) {
+			if(getTime()>o.getTime()){
+				return -1;
+			}else if(getTime()<o.getTime()){
+				return 1;
+			}
+			return 0;
+		}
+	}
+}
